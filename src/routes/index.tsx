916d23feecfc1160cabdd0b1dd794f8b4b0c8d74
yschoi <yschoi9733@gmail.com>
2025-11-24 14:07:31 +0900

    feat: 라우터에 모든 페이지 라우트 추가 (Join, Login, Catalog, Cart, Profile, Orders)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,50 @@
+import { Cart } from '@pages/Cart';
+import { Catalog } from '@pages/Catalog';
 import { Home } from '@pages/Home';
+import { Join } from '@pages/Join';
+import { Login } from '@pages/Login';
+import { MyOrder } from '@pages/MyOrder';
+import { Order } from '@pages/Order';
+import { Profile } from '@pages/Profile';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: 'join',
+    element: <Join />,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'catalog',
+    element: <Catalog />,
+  },
+  {
+    path: 'cart',
+    element: <Cart />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />,
+  },
+  {
+    path: 'orders',
+    children: [
+      {
+        index: true, // /orders
+        element: <Order />,
+      },
+      {
+        path: 'me', // /orders/me
+        element: <MyOrder />,
+      },
+    ],
   },
 ]);
 
