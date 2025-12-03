40c872e2a86201cd6f328308b5b6db2a4b08bfca
yschoi <yschoi9733@gmail.com>
2025-12-03 22:39:02 +0900

    feat: Add type annotations to catalog item API

--- src/api/catalog/item.api.ts
+++ src/api/catalog/item.api.ts
@@ -1,5 +1,11 @@
 import { httpClient } from '@/api/mainInstance';
+import { ItemDetail } from '@/types/item';
 
-export const getCatalogItemsAPI = async (itemCode: string) => {
-  return await httpClient.get(`catalog/items/${itemCode}`);
+export const getCatalogItemsAPI = async (
+  itemCode: string,
+): Promise<ItemDetail> => {
+  const response = await httpClient.get<ItemDetail>(
+    `catalog/items/${itemCode}`,
+  );
+  return response.data;
 };
